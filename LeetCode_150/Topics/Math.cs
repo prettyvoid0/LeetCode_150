2144f4dc5faa011a901bf2e849f2f81a9699d685
blust <blust.0.official@gmail.com>
2024-07-18 21:25:08 +0300

    SomeCompletedTasks

---
+++ LeetCode_150/Topics/Math.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode_150.Topics
+{
+    public class Math
+    {
+        /// <summary>
+        /// Given an integer x and checks if it is a palindrome.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>Returns True if x is a palindrome, else returns False.</returns>
+        public bool IsPalindrome(int x)
+        {
+            string strLeft = Convert.ToString(x);
+            string strRight = "";
+            for (int i = Convert.ToString(x).Length - 1; i != -1; i--)
+            {
+                strRight += strLeft[i];
+            }
+            if (strRight == strLeft)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCode_150/Topics/SingleDimensionalDP.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode_150.Topics
+{
+    public class SingleDimensionalDP
+    {
+        /// <summary>
+        /// Given the amount of stairs and returns an amount of distinct ways to climb to the top.
+        /// </summary>
+        /// <param name="n">The number of stairs.</param>
+        /// <returns>Returns the amount of distinct ways to climb to the top.</returns>
+        public int ClimbStairs(int n)
+        {
+            int result = 1;
+            int temp = 1;
+            for (int i = 1; i < n; i++)
+            {
+                result += temp;
+                temp = result - temp;
+            }
+            return result;
+        }
+    }
+}
