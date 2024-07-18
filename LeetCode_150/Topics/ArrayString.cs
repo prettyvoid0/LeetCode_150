6448902a879e9d0d26519a01c85db4a3cf00da69
blust <blust.0.official@gmail.com>
2024-07-18 09:11:53 +0300

    Completed task 169

--- LeetCode_150/Topics/ArrayString.cs
+++ LeetCode_150/Topics/ArrayString.cs
@@ -6,7 +6,7 @@
         /// Returns the amount of unique elements in the array.
         /// Set ununique elements after sorted list of unique elements for the array.
         /// </summary>
-        /// <param name="nums">The array of nums.</param>
+        /// <param name="nums">The array of integers.</param>
         /// <returns>The amount of unique elements in the array.</returns>
         public int RemoveDuplicates(int[] nums)
         {
@@ -26,7 +26,7 @@
         /// Removes the element from the array.
         /// Set other elements from the beginning of the array.
         /// </summary>
-        /// <param name="nums">The array of nums.</param>
+        /// <param name="nums">The array of integers.</param>
         /// <param name="val">The element which needs to be deleted.</param>
         /// <returns>The amount of remaining elements.</returns>
         public int RemoveElement(int[] nums, int val)
@@ -41,6 +41,34 @@
             nums = nums[0..counter];
 
             return nums.Length;
+        }
+
+        /// <summary>
+        /// Finds the most frequent element in the array.
+        /// </summary>
+        /// <param name="nums">The array of integers.</param>
+        /// <returns>The most frequent element in the array.</returns>
+        public int MajorityElement(int[] nums)
+        {
+            var detectedNums = new List<int>();
+            var frequency = new List<int>();
+
+            foreach (var num in nums)
+            {
+                if (detectedNums.Contains(num))
+                {
+                    var index = detectedNums.IndexOf(num);
+                    frequency[index]++;
+                }
+                else
+                {
+                    detectedNums.Add(num);
+                    frequency.Add(1);
+                }
+            }
+
+            var maxFrequencyIndex = frequency.IndexOf(frequency.Max(x => x));
+            return detectedNums[maxFrequencyIndex];
         }
     }
 }
