72528fa1e60d54ccfebdda5b508f13da655c7db1
blust <blust.0.official@gmail.com>
2024-07-19 08:46:25 +0300

    Completed task 392

--- LeetCode_150/Topics/TwoPointers.cs
+++ LeetCode_150/Topics/TwoPointers.cs
@@ -2,6 +2,11 @@
 {
     public class TwoPointers
     {
+        /// <summary>
+        /// Given a string and checks if it is a palindrome.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>Returns True if S is a palindrome, else returns False.</returns>
         public bool ValidPalindrome(string s)
         {
             s = String.Concat(Array.FindAll(s.ToCharArray(), Char.IsLetterOrDigit));
@@ -16,6 +21,45 @@
             }
 
             return invertedString.Equals(s);
+        }
+
+        /// <summary>
+        /// Given two strings and checks if s is a subsequence of t.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns>Returns True if S is a subsequence of T, else returns False.</returns>
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+
+            if (s.Length > t.Length)
+            {
+                return false;
+            }
+
+            var sList = s.ToList();
+            var tList = t.ToList();
+
+            bool flag = false;
+            for(int i = 0; i < tList.Count; i++)
+            {
+                if (sList[0] == tList[i])
+                {
+                    sList.RemoveAt(0);
+                }
+
+                if (sList.Count == 0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            return flag;
         }
     }
 }
