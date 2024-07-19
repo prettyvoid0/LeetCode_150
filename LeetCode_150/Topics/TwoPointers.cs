d2f9ab70e2f0a603d085effed9b748eb0be0b93c
blust <blust.0.official@gmail.com>
2024-07-19 08:35:25 +0300

    Completed task 125

---
+++ LeetCode_150/Topics/TwoPointers.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode_150.Topics
+{
+    public class TwoPointers
+    {
+        public bool ValidPalindrome(string s)
+        {
+            s = String.Concat(Array.FindAll(s.ToCharArray(), Char.IsLetterOrDigit));
+
+            s = s.ToLower();
+
+            string invertedString = "";
+
+            for (int i = s.Length - 1; i!=-1; i--)
+            {
+                invertedString += s[i];
+            }
+
+            return invertedString.Equals(s);
+        }
+    }
+}
